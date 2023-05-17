09acfae833cd9b4c7df50b37ecbab67a0666d988
msandypr <msandypr@gmail.com>
2023-05-17 12:09:28 +0700

    Creating Controllers, Models, and Routes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import db from "./config/Database.js";
+import Users from "./models/UserModel.js";
+import router from "./routes/index.js";
+
 const app = express();
 
 try{
     await db.authenticate();
     console.log('Database Successfuly Connected');
+    await Users.sync(); //Automatically generate table if not exists
 } catch(error){
     console.error(error);
 }
+
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => console.log('Server running on port 5000'));
---
+++ models/UserModel.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Users = db.define('user_account',{
+    username:{
+        type: DataTypes.STRING
+    },
+    email:{
+        type: DataTypes.STRING
+    },
+    password:{
+        type: DataTypes.STRING
+    },
+    refresh_token:{
+        type: DataTypes.TEXT
+    }
+},{
+    freezeTableName: true
+});
+
+export default Users;
